525774c91c0bed7489332d4556bb380f54c6a3ac
tidianecheick <tidicheick02@gmail.com>
2023-04-13 21:13:46 +0000

    first commit

---
+++ src/main/java/com/axel/gesproduit/GesproduitApplication.java
@@ -0,0 +1,13 @@
+package com.axel.gesproduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesproduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesproduitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axel/gesproduit/GesproduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axel.gesproduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesproduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
